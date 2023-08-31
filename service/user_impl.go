a7ca0133ce9d4a3b1d5f2277b40bf9b0872f671b
magnetopython <2640520498@qq.com>
2023-08-31 23:20:28 +0800

    Update user_impl.go

--- service/user_impl.go
+++ service/user_impl.go
@@ -1,5 +1,16 @@
 package service
 
+import "douyinbackend/dao"
+
 type UserRegisterInfo struct {
-	Token string
+	UserID int64  `json:"user_id"`
+	Token  string `json:"token"`
+}
+
+func UserRegister(username string, password string) {
+	var err error
+	var info UserRegisterInfo
+	var token string
+	var user *dao.User
+
 }
